b8d7a8d6e150eb39daf00a8d2f1662337a3f0159
FutureWebClient <kharispowell@hotmail.com>
2024-05-28 12:53:40 -0500

    Delete auth.js

--- auth.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const isLoggedIn = localStorage.getItem('isLoggedIn');
-    if (!isLoggedIn) {
-        window.location.href = 'login.html';
-    }
-});
